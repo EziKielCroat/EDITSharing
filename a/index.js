704697a3f3ef9a4cf7ef4c0f98b9551abc57f532
EziKielCroat <alahairlines@gmail.com>
2023-02-22 17:13:31 +0100

    bitne sitnice i spojia glavni app

--- a/index.js
+++ a/index.js
@@ -1,9 +1,31 @@
 M.AutoInit();
 
-const dropzone = document.getElementById('drag-drop');
+console.log(localStorage.getItem('idkorisnika'));
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBynaVPHtPVT2u33R9Du3wJaWs694Cvi4w",
+  authDomain: "editsharing-38e1f.firebaseapp.com",
+  projectId: "editsharing-38e1f",
+  storageBucket: "editsharing-38e1f.appspot.com",
+  messagingSenderId: "137256725440",
+  appId: "1:137256725440:web:d2e074d22b10ffe3a9f625",
+  measurementId: "G-40DV23GZLC"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.firestore();
+
+setTimeout(() => {
+  if(localStorage.getItem('idkorisnika') == null) {
+    window.location.href = '/';
+  }
+}, '100');
+
 let mode = '';
 
-// postavljanje ondrop i click eventova(da mos i clicknit pa izade prozor i droppat file)
+function inputFunctions() {
+  // sva logika za input prozor
+const dropzone = document.getElementById('drag-drop');
 
 dropzone.addEventListener('click', () => {
   const input = document.createElement('input');
@@ -23,51 +45,28 @@ dropzone.addEventListener('drop', (event) => {
 dropzone.addEventListener('dragover', (event) => {
   event.preventDefault();
 });
+}
+
 
 function handleFiles(files) { // mode je varijabla koju san stavija cisto da nepisen zasbne funkcije za sve 
 if(mode == 'p2p') {
     if (files.length > 0) {
         const file = files[0];
-        if (isCompressedFile(file)) {
           document.getElementById('drag-drop').innerText = `Odabrana datoteka: ${file.name}`
           document.getElementById('submitButton').classList.remove('disabled');
           p2pHandler(file);
-        } else {
-          console.log('Nedozvoljen tip datoteke');
-        }
       }
-} else if(mode == "s2p") {
+} else if(mode == 's2p') {
     if (files.length > 0) {
         const file = files[0];
-        if (isCompressedFile(file)) {
             document.getElementById('drag-drop').innerText = `Odabrana datoteka: ${file.name}`
             document.getElementById('submitButton').classList.remove('disabled');
             s2pHandler(file);
-        } else {
-          console.log('Nedozvoljen tip datoteke');
-        }
       }
 } else {
     alert('Kritična pogreška.');
     window.location.reload()
 }
-}
-
-function isCompressedFile(file) {
-  const compressedTypes = [
-    'application/octet-stream',
-    'application/zip',
-    'application/x-rar-compressed',
-    'application/x-7z-compressed',
-    'application/x-zip-compressed',
-    'application/x-tar',
-    'application/gzip',
-    'application/x-gzip',
-    'application/x-rar-compressed',
-    'application/x-compressed',
-  ];
-  console.log(file);
-  return compressedTypes.includes(file.type);
 }
 
 // Početak aplikacije
@@ -80,32 +79,77 @@ function s2pHandler(file) {
 
 }
 
-
-
 // Pomoćne funkcije
 
 //Obije funkcije samo daju mode varijabli vrijednost i appenda input stvar
 function p2pMode() {
-    mode = "p2p";
+    mode = 'p2p';
     let inputHolder = document.createElement('div');
-    inputHolder.setAttribute("class", "input-holder");
+    inputHolder.setAttribute('class', 'input-holder');
     inputHolder.innerHTML = '<div id="drag-drop">Stisni ili ubaci datoteku koju želiš podjeliti</div><button class="btn waves-effect red lighten-1 disabled" type="submit" name="action" id="submitButton">Djeli<i class="material-icons right">send</i></button>'
-    document.getElementsByClassName("container")[0].appendChild(inputHolder);
-    document.getElementsByClassName("input-holder")[0].style.display = "block";
+    document.getElementsByClassName('container')[0].appendChild(inputHolder);
+
+    inputFunctions();
+
+    document.getElementsByClassName('input-holder')[0].style.display = 'block';
 }
 
 function s2pMode() {
-    mode = "s2p";
+    mode = 's2p';
     let inputHolder = document.createElement('div');
     inputHolder.setAttribute("class", "input-holder");
     inputHolder.innerHTML = '<div id="drag-drop">Stisni ili ubaci datoteku koju želiš podjeliti</div><button class="btn waves-effect red lighten-1 disabled" type="submit" name="action" id="submitButton">Djeli<i class="material-icons right">send</i></button>'
     document.getElementsByClassName("container")[0].appendChild(inputHolder);
-    document.getElementsByClassName("input-holder")[0].style.display = "block";
+    inputFunctions();
+    document.getElementsByClassName('input-holder')[0].style.display = 'block';
 }
 
 // otvara question modal // refactorat modal mozda
 function questionModal() {
-    const elem = document.getElementById('modal1');
-    const instance = M.Modal.init(elem, {dismissible: false});
-    instance.open();
+  const elem = document.getElementById('modal1');
+  const instance = M.Modal.init(elem, {dismissible: false});
+  instance.open();
 }
+
+
+function accountModal() {
+  const elem = document.getElementById('modal2');
+  const instance = M.Modal.init(elem, {dismissible: false});
+  instance.open();
+}
+
+function signOut() {
+  localStorage.removeItem('idkorisnika');
+  window.location.href = '/';
+}
+
+function promjeniIme() {
+  let usernameNew = document.getElementById('promjenaImena').value;
+
+  if(usernameNew.length > 0) {
+    db.collection('Korisnici').doc(localStorage.getItem('idkorisnika')).update({
+      username: `${usernameNew}`
+    });
+    document.getElementById('promjenaImena').value == '';
+
+  }else {
+    alert('Upišite validno ime');
+    document.getElementById('promjenaImena').value == '';
+  }
+}
+
+function promjeniLozinku() {
+  let passwordNew = document.getElementById('promjenaLozinke').value;
+
+  if(passwordNew.length > 0) {
+    db.collection('Korisnici').doc(localStorage.getItem('idkorisnika')).update({
+      password: `${passwordNew}`
+    });
+    document.getElementById('promjenaLozinke').value == '';
+    
+  }else {
+    alert('Upišite validno prezime');
+    document.getElementById('promjenaLozinke').value == '';
+  }
+}
+
--- main.js
+++ main.js
@@ -15,7 +15,16 @@ const db = app.firestore();
 let loginButton = document.getElementById("loginButton");
 let registerButton = document.getElementById("registerButton");
 
-  // Ulogiranje korisnika I izrada novog računa korisnika
+
+// cekiranje jel korisnik već ulogiran ako je prosljedi ga na glavnu app
+setTimeout(() => {
+    if(localStorage.getItem("idkorisnika") !== null) {
+      window.location.href = "/a/index.html";
+    }
+}, "100");
+
+
+// Ulogiranje korisnika I izrada novog računa korisnika
 
 loginButton.addEventListener("click", function() {
     let username = document.getElementById("username").value;
@@ -26,8 +35,7 @@ loginButton.addEventListener("click", function() {
             querySnapshot.forEach(doc => {
                 let obj = doc.data(); // obj je objekt koji nije prazan ako je nasa korisnika koji se podudara u db
                 if (Object.keys(obj).length > 0) {
-                    glavnaAplikacija();
-                    console.log(obj); // ovdje ide logika za nastavit aplikaciju u dio di mos djelit dokumente
+                    glavnaAplikacija(doc.id);
                 } else {
                     alert("Račun sa upisanim korisničkim imenom i lozinkom ne postoji.")
                 }
@@ -42,34 +50,37 @@ registerButton.addEventListener("click", function() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if(username.length > 0 && password.length > 0) {
+    if(username.length > 0 && password.length > 0) { 
         db.collection("Korisnici").add({
             username: `${username}`,
             password: `${password}`,
 
         }).then((docRef) => {
             // ovdi dodat logiku za nastavljanje aplikacije
-            glavnaAplikacija();
             console.log("Document written with ID: ", docRef.id);
+            glavnaAplikacija(docRef.id);
         })
         .catch((error) => {
             console.error("Error adding document: ", error);
         });
     } else {
-        alert("Upišite validno korisničko ime i lozinku.")
+         alert("Upišite validno korisničko ime i lozinku.")
     }
 });
 
 // Glavne funkcije
 
-function glavnaAplikacija() {
-    alert("OK!"); // ovdje ide glavna aplikacija
+function glavnaAplikacija(idKorisnika) {
+    // idKorisnika stavljan u webstorage tako da se moze provjerit u glavnoj app jel korisnik ulogiran
+    localStorage.setItem("idkorisnika", idKorisnika);
+
+    window.location.href = "/a/index.html";
 }
 
-  // Otvaranje Question Modala ako korisnika zanima što i kako naša aplikacija osoba.
-function questionModal() {
+// Pomoćne funkcije
+
+function questionModal() { // otvara modal definiran u htmlu
     const elem = document.getElementById('modal1');
     const instance = M.Modal.init(elem, {dismissible: false});
     instance.open();
 }
-
